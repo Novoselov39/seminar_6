d2d06ca1f5474136c0c02415b1e924cc133c8aa9
Anatolii <novoselov39@gmail.com>
2023-06-09 20:28:20 +0300

    Seminar 6

---
+++ task_41/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+int[] ReadMass()
+{
+    Console.WriteLine("Введите размерность массива:");
+    int i, n = Convert.ToInt32(Console.ReadLine());
+    int[] mas = new int[n];
+    Console.WriteLine("Введите массив:");
+    string[] str = Console.ReadLine().Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+    for (i = 0; i < (n < str.Length ? n : str.Length); ++i)
+        mas[i] = Convert.ToInt32(str[i]);
+    return mas;
+}
+
+void CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    Console.WriteLine($"Количество положительных эллементов массива = {count}");
+}
+
+CountPositiveNumbers(ReadMass());
+
---
+++ task_43/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+bool stat= true;
+if (k1 == k2) 
+{
+        Console.WriteLine("Прямые параллельны!!");
+        stat=false;
+        
+}
+
+double[] FindCoordinates(double b1, double k1, double b2, double k2)
+{
+    //y = k1 * x + b1, y = k2 * x + b2
+    double[] cord = new double[2];    
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    cord[0] = x;
+    cord[1] = y;
+    return cord;
+}
+
+if (stat) Console.WriteLine($" Координаты [{String.Join(" ", FindCoordinates(b1, k1, b2, k2))}]");
